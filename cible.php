b3aceace9cdf6709a9ad20119dc6e3c92a394e33
David-Charlesia <pro.charlesia.david@outlook.com>
2020-08-04 17:57:45 +0200

    remise en forme

---
+++ cible.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p>Bonjour !</p>
+
+    <p>Je sais comment tu t'appelles, hé hé. Tu t'appelles <?php echo htmlspecialchars($_POST['prenom']); ?> !</p>
+
+    <p>Si tu veux changer de prénom, <a href="index.php">clique ici</a> pour revenir à la page formulaire.php.</p>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p>
+    Cette page ne contient que du HTML.<br />
+    Veuillez taper votre prénom :
+  </p>
+
+  <form action="cible.php" method="post">
+    <p>
+      <input type="text" name="prenom" />
+      <input type="submit" value="Valider" />
+    </p>
+  </form>
+
+</form>
+  </body>
+</html>
